23bc58f954119f50038557a6a849fade42336deb
Jung Hwan Park <hwann1005@gmail.com>
2023-01-16 22:26:09 +0900

    remove comments...

--- Autocrypt_JungHwan/Networking/URLRequest_Extensions.swift
+++ Autocrypt_JungHwan/Networking/URLRequest_Extensions.swift
@@ -19,27 +19,6 @@ extension URLRequest
 {
     static func load<T: Decodable>(resource: Resource<T>) -> Observable<T>
     {
-//        return Observable.just(resource.url)
-//            .flatMap { url -> Observable<(HTTPURLResponse)> in
-//                let request = URLRequest(url: url)
-//                return AF.request(resource.url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil, interceptor: nil, requestModifier: nil).responseData { response -> T in
-//                    switch response.result
-//                    {
-//                        case .success(let data):
-//                            do
-//                            {
-//                                try JSONDecoder().decode(T.self, from: data)
-//                            }
-//                            catch
-//                            {
-//                                
-//                            }
-//                    case .failure(let error): break
-//                            
-//                    }
-//                }
-//            }.asObservable()
-        
         return Observable.create
         { observer in
             let request = AF.request(resource.url, method: .get, parameters: nil, encoding: URLEncoding.default, headers: nil, interceptor: nil, requestModifier: nil).responseData
@@ -67,23 +46,4 @@ extension URLRequest
             }
         }
     }
-    
-//    static func load<T: Decodable>(resource: Resource<T>) -> Observable<T>
-//    {
-//        return Observable.just(resource.url).flatMap
-//        { url -> Observable<(response: HTTPURLResponse, data: Data)> in
-//            let request = URLRequest(url: url)
-//            return URLSession.shared.rx.response(request: request)
-//        }.map
-//        { response, data -> T in
-//            if 200 ..< 300 ~= response.statusCode
-//            {
-//                return try JSONDecoder().decode(T.self, from: data)
-//            }
-//            else
-//            {
-//                throw AFError.explicitlyCancelled
-//            }
-//        }.asObservable()
-//    }
 }
